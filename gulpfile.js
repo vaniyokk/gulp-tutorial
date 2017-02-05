25d0290a893d02035439583b284e558ad3083cae
Ivan Kuzmin <kuzmin6969@gmail.com>
2017-02-05 19:50:45 +0200

    Add image optimization, font transfer and new tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 var gulp = require('gulp');
+
 var sass = require('gulp-sass');
 var bs = require('browser-sync').create();
 var useref = require('gulp-useref');
-
 var uglify = require('gulp-uglify');
 var cssnano = require('gulp-cssnano');
 var gulpIf = require('gulp-if');
 var eol = require('gulp-eol');
-
-gulp.task('hello', function () {
-    console.log('hello');
-});
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var del = require('del');
+var runSequence = require('run-sequence');
 
 gulp.task('sass', function() {
     return gulp.src('app/scss/**/*.scss')
@@ -21,8 +21,7 @@ gulp.task('sass', function() {
         }));
 });
 
-gulp.task('watch', ['bs', 'sass'], function() {
-    console.log('in WATCH');
+gulp.task('watch', function() {
     gulp.watch('app/scss/**/*.scss', ['sass', 'useref']);
     gulp.watch('app/*.html', function() {
         gulp.run('useref');
@@ -38,11 +37,40 @@ gulp.task('bs', function() {
     });
 });
 
-gulp.task('useref', ['sass'], function() {
+gulp.task('useref', function() {
     return gulp.src('app/*.html')
         .pipe(useref())
         .pipe(gulpIf('*.css', cssnano()))
         .pipe(gulpIf('*.js', uglify()))
         .pipe(gulp.dest('dist'));
+});
+
+gulp.task('images', function () {
+    return gulp.src('app/images/**/*.+(gif|jpg|jpeg|png|svg)')
+        .pipe(cache(imagemin({
+            optimizationLevel: 7
+        }))
+        .pipe(gulp.dest('dist/images')))
+});
+
+gulp.task('fonts', function() {
+    return gulp.src('app/fonts/**/*')
+        .pipe(gulp.dest('dist/fonts'))
+});
+
+gulp.task('clean', function () {
+   return del.sync(['dist', 'app/css']); 
+});
+
+gulp.task('default', function () {
+    runSequence('bs', 'sass', 'watch');
+});
+
+gulp.task('build', function () {
+    runSequence('clean:dist', 'sass', ['useref', 'images', 'fonts'], 
+        function() {
+            console.log('We are ready for production!')
+        }
+    );
 });
 
